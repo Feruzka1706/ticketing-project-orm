4baf85742aa3e8bc6a4282bfdd441ea6f4a82ae6
feruzka1706@gmail.com <feruzka1706@gmail.com>
2023-11-19 18:21:10 -0600

    Part 3 code

--- src/main/java/com/cydeo/entity/BaseEntity.java
+++ src/main/java/com/cydeo/entity/BaseEntity.java
@@ -16,9 +16,14 @@ public class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false,updatable = false)
     private LocalDateTime insertDateTime;
+    @Column(nullable = false,updatable = false)
     private Long insertUserId;
+    @Column(nullable = false)
     private LocalDateTime lastUpdateDateTime;
+    @Column(nullable = false)
     private Long lastUpdateUserId;
 
     private Boolean isDeleted=false;
@@ -29,6 +34,13 @@ public class BaseEntity {
         this.insertDateTime= LocalDateTime.now();
         this.lastUpdateDateTime=LocalDateTime.now();
         this.insertUserId=1L;
+        this.lastUpdateUserId=1L;
+    }
+
+
+    @PreUpdate
+    public void onPreUpdate(){
+        this.lastUpdateDateTime=LocalDateTime.now();
         this.lastUpdateUserId=1L;
     }
 
