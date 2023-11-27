6f19b0656d4af11035483cfd7fe8074d7e9f3d84
feruzka1706@gmail.com <feruzka1706@gmail.com>
2023-11-26 19:59:46 -0600

    Ticketing Application Spring Security code changes

--- src/main/java/com/cydeo/TicketingProjectOrmApplication.java
+++ src/main/java/com/cydeo/TicketingProjectOrmApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.context.MessageSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication(exclude = MessageSourceAutoConfiguration.class)
 public class TicketingProjectOrmApplication {
@@ -25,4 +27,10 @@ public class TicketingProjectOrmApplication {
     public ModelMapper mapper(){
         return new ModelMapper();
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
 }
---
+++ src/main/java/com/cydeo/config/SecurityConfig.java
@@ -0,0 +1,65 @@
+package com.cydeo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class SecurityConfig {
+
+    /**
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder){
+
+        //we want to create more than 1 user
+        List<UserDetails> userList = new ArrayList<>();
+        userList.add(
+        new User("mike",encoder.encode("password"),
+                Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN"))));
+
+        userList.add( new User("ozzy",encoder.encode("password"),
+                Arrays.asList(new SimpleGrantedAuthority("ROLE_MANAGER"))));
+
+       return new InMemoryUserDetailsManager(userList);
+
+    }
+    */
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+        return httpSecurity
+                .authorizeRequests()
+                .antMatchers("/user/**").hasRole("ADMIN")  //ROLE_ADMIN
+                .antMatchers("/project/**").hasRole("MANAGER")
+                .antMatchers("/task/employee/**").hasRole("EMPLOYEE")
+                .antMatchers("/task/**").hasRole("MANAGER")
+               //.antMatchers("/task/**").hasAnyRole("EMPLOYEE","ADMIN") //multiple ppl can have an access for this end points
+               //.antMatchers("/task/**").hasAuthority("ROLE_EMPLOYEE")
+                .antMatchers(
+                        "/",
+                        "/login",
+                        "/fragments/**",
+                        "/assets/**",
+                        "/images/**"
+                ).permitAll()
+                .anyRequest().authenticated()
+                .and()
+               // .httpBasic() //we don't want to use httpBasic() box
+                .formLogin()
+                    .loginPage("/login")
+                    .defaultSuccessUrl("/welcome")
+                    .failureUrl("/login?error=true")
+                    .permitAll()
+                .and().build();
+    }
+
+
+}
